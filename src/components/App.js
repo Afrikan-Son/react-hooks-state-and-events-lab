01e8680118cb97654eeec45d0917875da3419131
Mark Wanjau <theafrikanson@gmail.com>
2023-04-25 04:58:43 +0300

    pass app.js test

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-  const appClass = isDarkMode ? "App dark" : "App light";
+	// replace 'false' with a state variable that can be toggled between true and false
+	// this will be used for the Dark Mode Toggle feature
 
-  return (
-    <div className={appClass}>
+	const [isToggled, setIsToggled] = useState(false);
 
-      {/* function App() {
+	// setIsToggled(!isToggled)   onClick={()=>setIsTobled(!isToggled}
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
+	const appClass = isToggled ? "App dark" : "App light";
 
-  const [isToggled, setIsToggled] = useState(false);
-  const appClass = isToggled ? "App dark" : "App light";
-
-
-
-  return (
-    <div className={appClass}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={() => setIsToggled(!isToggled)}>Dark Mode</button>
-      </header>
-      <ShoppingList items={itemData} />
-    </div>
-  );
+	return (
+		<div className={appClass}>
+			<header>
+				<h2>Shopster</h2>
+				<button onClick={() => setIsToggled(!isToggled)}>Dark Mode</button>
+			</header>
+			<ShoppingList items={itemData} />
+		</div>
+	);
 }
-*/}
-
-      <header>
-        
-        <h2>Shopster</h2>
-        <button onClick={() => setIsDarkMode(!isDarkMode)}>Dark Mode</button>
-      </header>
-      <ShoppingList items={itemData} />
-    </div>
-  );
-}
-
 
 export default App;
-
-
